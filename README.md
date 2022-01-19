0f58a302061eb9e0f17c6a88e6f7b8ea5b04bac0
Andrea Arias <ariasdrea@gmail.com>
2022-01-19 18:04:41 +0100

    v2

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spotify API
+
+## Live Website
+
+https://ariasdrea.github.io/Spotify-API/
+
+## Overview
+
+An interface that allows users to search their favorite artists and albums by making AJAX calls to Spotify's API via a proxy. Clicking any search result will redirect the user to the related artist/album's Spotify page.
+
+## Features
+
+-   A message will display if there are no results for a search.
+-   A default image will display for results that do not have any images.
+-   Infinite scroll is possible only when the string "scroll=infinite" appears in the query string when the page loads. Infinite scroll allows the user to scroll down to the bottom of the listed results and the next page of results will automatically be loaded and appended to the latest results. As part of this functionality, the 'More' button disappears.
+
+## Technology
+
+-   jQuery & AJAX
+
+## Preview
+
+**_Introduction Page & Registration_**
+![](registration.gif)
---
+++ script.js
@@ -0,0 +1,100 @@
+(function() {
+    var go = $("#go");
+    var input = $("input");
+    var select = $("select");
+    var offsetStart = 0;
+    var more = $("#more");
+
+    // GET RESULTS FUNCTION
+    function getMoreResults(clickGo) {
+        $.ajax({
+            url: "http://spicedify.herokuapp.com/spotify",
+            method: "GET",
+            data: {
+                q: input.val(),
+                type: select.val(),
+                offset: offsetStart
+            },
+            success: function(data) {
+                data = data.artists || data.albums;
+                
+                console.log('data from spotify: ', data);
+
+                if (data.next) {
+                    offsetStart += 20;
+                    $("#more").show();
+                } else {
+                    $("#more").hide();
+                }
+
+                // IMAGES
+                var resultArr = [];
+                for (var i = 0; i < data.items.length; i++) {
+
+                    var img = "default.png";
+                    if (data.items[i].images[0]) {
+                        img = data.items[i].images[0].url;
+                    }
+
+                    resultArr.push({
+                        externalUrl: data.items[i].external_urls.spotify,
+                        image: img,
+                        name: data.items[i].name
+                    });
+                }
+
+                if (clickGo) {
+                    $('.results').html(Handlebars.templates.spotifyId({
+                        spotifyObj: resultArr
+                    }));
+                } else {
+                    $('.results').append(Handlebars.templates.spotifyId({
+                        spotifyObj: resultArr
+                    }));
+                }
+
+                if (!data.items.length) {
+                    $(".results").html(`No results found for "${input.val()}"`);
+                }
+
+                if (location.search.indexOf("scroll=infinite") > -1) {
+                    checkInfiniteScroll();
+                    more.hide();
+                }
+            }
+        });
+    }
+
+    // CLICK ON GO
+    go.on("click", function() {
+        getMoreResults(true);
+    });
+
+    // CLICK ON MORE
+    more.on("click", function() {
+        getMoreResults();
+    });
+
+    // INFINITE SCROLL
+    var timeoutId;
+    function checkInfiniteScroll() {
+        clearTimeout(timeoutId);
+        if (
+            $(window).height() + $(document).scrollTop() >=
+            $(document).height() - 500
+        ) {
+            getMoreResults();
+        } else {
+            timeoutId = setTimeout(checkInfiniteScroll, 1000);
+        }
+    }
+
+    ///////// HB BOILERPLATE CODE /////////
+    Handlebars.templates = Handlebars.templates || {};
+
+    var templates = document.querySelectorAll('script[type="text/x-handlebars-template"]');
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+})();
